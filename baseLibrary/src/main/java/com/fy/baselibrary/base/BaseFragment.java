bdb3890b0b5a68014be182a2f8cf6cabb96ebbbf
fangs <fy310518@163.com>
2018-03-21 09:23:26 +0800

    增加 fragment生命周期 回调

--- baseLibrary/src/main/java/com/fy/baselibrary/base/BaseFragment.java
+++ baseLibrary/src/main/java/com/fy/baselibrary/base/BaseFragment.java
@@ -6,6 +6,7 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.app.AppCompatDelegate;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,6 +21,7 @@ import butterknife.Unbinder;
  * Created by fangs on 2017/4/26.
  */
 public abstract class BaseFragment extends Fragment implements View.OnClickListener {
+    public static final String TAG = "Fragment";
 
     protected ACache mCache;
 
@@ -29,14 +31,6 @@ public abstract class BaseFragment extends Fragment implements View.OnClickListe
 
     static {
         AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
-    }
-
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-
-        this.mContext = (AppCompatActivity) context;
-        mCache = ACache.get(mContext);
     }
 
     @Nullable
@@ -53,6 +47,7 @@ public abstract class BaseFragment extends Fragment implements View.OnClickListe
                 parent.removeView(mRootView);
             }
         }
+        Log.e(TAG, "onCreateView()");
         return mRootView;
     }
 
@@ -65,13 +60,7 @@ public abstract class BaseFragment extends Fragment implements View.OnClickListe
 
     protected abstract int setContentLayout();
 
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        if (null != unbinder){
-            unbinder.unbind();
-        }
-    }
+
 
     //Fragment生命周期管理
     @Override
@@ -82,5 +71,74 @@ public abstract class BaseFragment extends Fragment implements View.OnClickListe
         } else {// 重新显示到最前端中
             onResume();
         }
+    }
+
+
+    @Override//当Activity中的onCreate方法执行完后调用
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        Log.e(TAG, "onActivityCreated()");
+    }
+
+    @Override//Fragment和Activity建立关联的时候调用
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        Log.e(TAG, "onAttach()");
+
+        this.mContext = (AppCompatActivity) context;
+        mCache = ACache.get(mContext);
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.e(TAG, "onCreate()");
+    }
+
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.e(TAG, "onStart()");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.e(TAG, "onResume()");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.e(TAG, "onPause()");
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.e(TAG, "onStop()");
+    }
+
+    @Override//Fragment中的布局被移除时调用
+    public void onDestroyView() {
+        super.onDestroyView();
+        Log.e(TAG, "onDestroyView()");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.e(TAG, "onDestroy()");
+
+        if (null != unbinder){
+            unbinder.unbind();
+        }
+    }
+
+    @Override//Fragment和Activity解除关联的时候调用
+    public void onDetach() {
+        super.onDetach();
+        Log.e(TAG, "onDetach()");
     }
 }
